82d32123b2cddcc94366fa8cf8426930b6f04898
Dario Corrada <dario.corrada@gmail.com>
2014-06-02 21:09:58 +0200

    DBI collision fixed

--- QUEST/QUEST.server.pl
+++ QUEST/QUEST.server.pl
@@ -54,6 +54,8 @@ our $poweroff; # la variabile gestita dalla suboutine &sigIntHandler
 # THREADING
 our $semaforo; # semaforo basato per il blocco dei threads 
 our $dbaccess :shared; # accesso esclusivo al database
+our @slotstatus :shared; # stato degli slot
+our @slotobj; # lista degli oggetti 'threads' lanciati (ie gli slot)
 
 # OTHERS
 our $superuser; # il server è stato lanciato come superuser?
@@ -145,12 +147,21 @@ INIT: {
     # inizializzo il semaforo
     $semaforo = Thread::Semaphore->new(int($confs{'threads'}));
     
+    # lancio gli slot, il numero di slot aperti equivale al numero di threads (ie numero massimo di thread concorrenti)
+    for (my $i = 0; $i < $confs{'threads'}; $i++) {
+        my $thr = threads->new(\&superslot, $i);
+#         $thr->detach();
+        $slotobj[$i] = $thr; # raccolgo l'oggetto 'threads' in un array
+#         printf("%s slot %d opened\n", clock(), $i);
+    }
+# threads->new(\&superslot, 1);
+# sleep 1;
+# threads->new(\&superslot, 2);
+    
     printf("%s server initialized\n\n", clock());
     print "    CONFIGS...: $conf_file\n";
     print "    DATABASE..: $database\n\n";
 }
-
-goto FINE;
 
 CORE: {
     # apro un socket per comunicare con il client
@@ -179,172 +190,14 @@ END
             my $recieved_data;
             $client->recv($recieved_data,1024);
             
-            # richiesta della lista dei job
-            if ($recieved_data eq 'status') {
-                my $log = job_monitor();
-                $client->send($log);
-                
-            # richiesta di uccidere job
-            } elsif ($recieved_data =~ /killer/ ) {
-                my ($jobid, $user_client) = $recieved_data =~ /killer\|([\w\d]+);user\|([\w\d]+)/;
-                my $mess;
-                
-                if (exists $killerlist{$jobid}) {
-                    my $job = $killerlist{$jobid};
-                    if ($job->is_running) {
-                        
-                        # verifico le credenziali
-                        unless ($superuser && ($user_client eq 'root')) {
-                            my $match;
-                            {
-                                lock @queued; lock @running;
-                                my @joblist;
-                                push(@joblist, @queued, @running);
-                                ($match) = grep(/$jobid/, @joblist);
-                            }
-                            my ($job_owner) = $match =~ /\]\s+([\w\d]+)/;
-                            unless ($user_client eq $job_owner) {
-                                $mess = <<END
-E- the owner of job [$jobid] is $job_owner, you are not allowed to kill it
-END
-                                ;
-                                goto ENDKILL;
-                            } 
-                        }
-                        
-                        printf("%s killing job [%s] requested by [%s]\n", clock(), $jobid, $user_client);
-                        
-                        # verifico se il job sta girando
-                        my $is_running = 'null';
-                        {
-                            lock @running;
-                            my ($greppo) = grep(/$jobid/, @running);
-                            if ($greppo) {
-                                $is_running = $greppo;
-                                
-                                # verifico se si tratta di un job Schrodinger
-                                if ($greppo =~ /\[Schrodinger: /) {
-                                    my ($schrodinger_jobid) = $greppo =~ /\[Schrodinger: (.+)\]/;
-                                    $mess = <<END
-
-killing the Schrodinger's cat is unsafe, try to use the following commad:
-
-    \$ \$SCHRODINGER/jobcontrol -kill $schrodinger_jobid
-END
-                                    ;
-                                    goto ENDKILL;
-                                }
-                            }
-                        }
-                        
-                        # ammazzo eventuali processi figli se il job sta girando
-                        unless ($is_running eq 'null') {
-                            my $parent_pid;
-                            {
-                                lock @running;
-                                my ($greppo) = grep(/$jobid/, @running);
-                                ($parent_pid) = $greppo =~ /\[PID: (\d+)\]/;
-                                unless ($parent_pid) {
-                                    $mess = "E- unable to find PID for job [$jobid]";
-                                    goto ENDKILL;
-                                }
-                            }
-                            undef @children;
-                            push (@children, $parent_pid);
-                            &getcpid($parent_pid);
-                            @children = sort {$b <=> $a} @children;
-                            while (my $child = shift(@children)) {
-                                print "\tkilling child pid $child...\n";
-                                kill 15, $child;
-                                sleep 1; # aspetto un poco...
-                            }
-                        }
-                        $job->kill('TERM');
-                        
-                        # verifico se il job è stato ucciso
-                        sleep 4.6;
-                        
-                        if ($job->is_running) {
-                            $mess = "W- job [$jobid] is still alive, try to check it later";
-                        } else {
-                            $mess = "job [$jobid] killed";
-                        }
-                        
-                    } else {
-                        $mess = "job [$jobid] has already accomplished";
-                    }
-                } else {
-                    $mess = "E- job [$jobid] not found";
-                }
-                
-                ENDKILL: { $client->send($mess); };
-                
-            # richiesta di sottomettere job
-            } elsif ($recieved_data =~ /user/ ) {
-                
-                $submitted++;
-                if ($submitted > $confs{'maxsub'}) {
-                    my $mess = <<END
-WARNING: the server has collected $confs{'maxsub'} jobs. You should restart the 
-server before submitting another job.
-END
-                    ;
-                    $client->send($mess);
-                    $client->send('QUEST.over&out');
-                    next;
-                } else {
-                    my $mess = sprintf(">>> %05d submissions to restart <<<\n\n", $confs{'maxsub'} - $submitted);
-                    $client->send($mess);
-                }
-                
-                my @params = split(';', $recieved_data);
-                my %client_order;
-                while (my $order = shift @params) {
-                    my ($key, $value) = $order =~ /(.+)\|(.+)/;
-                    $client_order{$key} = $value;
-                }
-                
-                # verifico se il server ha le credenziali per lanciare il job
-                my $username = $ENV{'USER'};
-                unless ($superuser) {
-                    unless ($client_order{'user'} eq $username) {
-                        my $mess = <<END
-JOB LAUNCH ABORTED: the server has been launched as as [$username] and only this
-user can submit job (not as [$client_order{'user'}]).
-END
-                        ;
-                        $client->send($mess);
-                    }
-                }
-                
-                # genero un jobid
-                my @chars = ('A'..'Z', 0..9, , 0..9);
-                my $jobid = join('', map $chars[rand @chars], 0..7);
-                
-                # genero un file di log in cui raccogliero' l'output del job
-                my $logfile = sprintf("%s/QUEST.job.%s.log", $client_order{'workdir'}, $jobid);
-                
-                # sottometto il job
-                my $job = threads->new(\&launch_thread,
-                    $client_order{'script'},
-                    $logfile,
-                    $client_order{'threads'},
-                    $client_order{'user'},
-                    $jobid,
-                    $client_order{'workdir'},
-                    $client_order{'schrodinger'},
-                    $client_order{'queue'}
-                );
-                # $job->detach();
-                
-                # metto il job nella killerlist
-                $killerlist{$jobid} = $job;
-                
-                printf("%s job [%s] submitted by [%s]\n", clock(), $jobid, $client_order{'user'});
-                my $mess = sprintf("%s job [%s] queued,\nSTDOUT/STDERR will be written to <%s>",
-                    clock(), $jobid, $logfile);
-                $client->send($mess);
-                
+            if ($recieved_data eq 'status') { # richiesta della lista dei job
+                $client->send("status request");
+            } elsif ($recieved_data =~ /killer/ ) { # richiesta di uccidere job
+                $client->send("kill request");
+            } elsif ($recieved_data =~ /user/ ) { # richiesta di sottomettere job
+                $client->send("submission request");
+            } elsif ($recieved_data =~ /details/ ) { # richiesta di dettagli su di un job
+                $client->send("details request");
             } else {
                 next;
             }
@@ -363,101 +216,93 @@ FINE: {
 }
 
 sub superslot {
-    my $jobid;
-    my $ref_row;
-    my $threads; 
+    my ($slotid) = @_;
+    my ($jobid, $threads);
+    my $slotdbobj = RICEDDARIO::lib::SQLite->new('database' => $database, 'log' => 0);
     
     while (1) {
-        {
+        my $slotdbh = $db_obj->access2db();
+        my $slotsth;
+        
+        while (1) {
             # con questo blocco la sub sta in standby fintanto che non trova un job disponibile a partire
             
-            lock $dbaccess;
-            
             # estraggo il primo job dalla lista degli accodati
-            $sth = $db_obj->query_exec( 'dbh' => $dbh, 
+            $slotsth = $slotdbobj->query_exec( 'dbh' => $slotdbh, 
                 'query' => 'SELECT * FROM queuelist'
             );
             my %queued;
-            while ($ref_row = $sth->fetchrow_hashref()) {
+            while (my $ref_row = $slotsth->fetchrow_hashref()) {
                 my $key = $ref_row->{'score'};
                 my $value = $ref_row->{'jobid'};
                 $queued{$key} = $value;
             }
-            $sth->finish();
+            $slotsth->finish();
+            
+            unless (%queued) { # non ci sono job accodati
+                sleep 0.25;
+                { lock @slotstatus; $slotstatus[$slotid] = 'empty'; }
+                next;
+            };
+            
             my @sorted = sort {$a cmp $b} keys %queued;
             $jobid = $queued{$sorted[0]};
             
             # verifico il numero di threads che richiede il job
-            $sth = $db_obj->query_exec( 'dbh' => $dbh, 
+            $slotsth = $slotdbobj->query_exec( 'dbh' => $slotdbh, 
                 'query' => 'SELECT jobid, threads FROM subdetails WHERE jobid = ?',
                 'bindings' => [ $jobid ]
             );
-            $ref_row = $sth->fetchrow_hashref();
-            $sth->finish();
+            my $ref_row = $slotsth->fetchrow_hashref();
+            $slotsth->finish();
             $threads = $ref_row->{'threads'};
             if ($threads > ${$semaforo}) {
-                sleep 1;
+                sleep 0.25;
+                { lock @slotstatus; $slotstatus[$slotid] = 'standby'; }
                 next;
             } else {
                 # rimuovo il job dalla queuelist se può partire
-                $sth = $db_obj->query_exec( 'dbh' => $dbh, 
+                $slotsth = $slotdbobj->query_exec( 'dbh' => $slotdbh, 
                     'query' => 'DELETE FROM queuelist WHERE jobid =?',
                     'bindings' => [ $jobid ]
                 );
-                $sth->finish();
+                $slotsth->finish();
+                { lock @slotstatus; $slotstatus[$slotid] = 'busy'; }
+                last;
             }
+            
         }
         
-        my $start_time = time;
-        for (1..$threads) { $semaforo->down() }; # occupo tanti threads quanti richiesti
-        {
-            # aggiorno il database
-            
-            lock $dbaccess;
-            
-            $sth = $db_obj->query_exec( 'dbh' => $dbh, 
-                'query' => 'UPDATE jobstatus SET status = "running" WHERE jobid =?',
-                'bindings' => [ $jobid ]
-            );
-            $sth->finish();
-            
-            # i campi 'pid' e 'schroid' li aggiornerò con il monitor
-            $sth = $db_obj->query_exec( 'dbh' => $dbh, 
-                'query' => 'INSERT INTO runlist (jobid, rundate) VALUES (?, ?)',
-                'bindings' => [ $jobid,  $start_time ]
-            );
-            $sth->finish();
-        }
+#         my $start_time = time;
+#         for (1..$threads) { $semaforo->down() }; # occupo tanti threads quanti richiesti
+#         {
+#             # aggiorno il database
+#             
+#             lock $dbaccess;
+#             
+#             $sth = $db_obj->query_exec( 'dbh' => $dbh, 
+#                 'query' => 'UPDATE jobstatus SET status = "running" WHERE jobid =?',
+#                 'bindings' => [ $jobid ]
+#             );
+#             $sth->finish();
+#             
+#             # i campi 'pid' e 'schroid' li aggiornerò con il monitor
+#             $sth = $db_obj->query_exec( 'dbh' => $dbh, 
+#                 'query' => 'INSERT INTO runlist (jobid, rundate) VALUES (?, ?)',
+#                 'bindings' => [ $jobid,  $start_time ]
+#             );
+#             $sth->finish();
+#         }
+#         
         
         
-        # *** DAFFARE ***
+        $slotdbh->disconnect;
     }
 }
 
 
-# questa subroutine serve per intercettare un segnale di interrupt e comunicarlo
-# allo script inizializzando il valore della variabile globale $poweroff
+# questa subroutine serve per intercettare un segnale di interrupt
 sub sigIntHandler {
-    print "\n";
-    {
-        lock @running;
-        foreach my $jobid (keys %killerlist) {
-            my $job = $killerlist{$jobid};
-            if ($job->is_running) {
-                my ($match) = grep(/$jobid/, @running);
-                $match && do {
-                    my ($pid) = $match =~ /(\[PID: \d+\])/;
-                    my $warn = <<END
-
-W- job [$jobid] was running while KILL signal arrived, check for accidental 
-orphans generated from $pid
-END
-                    ;
-                    print $warn;
-                }
-            }
-        }
-    }
     $poweroff = 1;
 }
 
